a7e8434e1f68f38a94e7eb6bc018f7d5cde6dcd8
hf73 <70991020+hf73@users.noreply.github.com>
2021-11-17 19:06:25 -0500

    Created add, edit, list items, items table

--- partials/nav.php
+++ partials/nav.php
@@ -40,6 +40,9 @@ require_once(__DIR__ . "/../lib/functions.php");
             <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
             <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
             <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
+            <li><a href="<?php echo get_url('admin/add_item.php'); ?>">Add Item</a></li>
+            <li><a href="<?php echo get_url('admin/edit_item.php'); ?>">Edit Item</a></li>
+            <li><a href="<?php echo get_url('admin/list_item.php'); ?>">List Item</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
